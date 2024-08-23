300dc3db06bc9a47b8fa2fafb7f0c62ee2c976d2
Yoshigamer360 <133695386+Yoshigamer360@users.noreply.github.com>
2024-08-23 11:31:52 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,8 @@ previousAge = int(input('When I was __ years old: '))
 previousTimeCount = int(input('__ weeks/months/years: '))
 timemeasurement = input('A) Weeks, B) Months, C) Years: ').lower()
 currentAge = int(input('Your current age: '))
+
+if timemeasurement == 'a':
+    fractionOfLife = 1 / (previousAge * 52 / previousTimeCount)
+    currentTimeCount = currentAge * 52 * fractionOfLife
+    print(f'When I was {previousAge} years old, {previousTimeCount} weeks feels as long as {currentTimeCount} weeks for a {currentAge} year old')
