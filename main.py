eb49a293cb31ed59578c4e267ee94674feaebf19
Yoshigamer360 <133695386+Yoshigamer360@users.noreply.github.com>
2024-08-23 11:09:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 # When I was __ years old, __ weeks/months/years feels as long as __ weeks/months/years as a __ years old
+
+print('Fill in the blanks in order')
+#10 1y 10y 100
+
+oldYear = int(input('When I was __ years old: '))
+oldTimecount = int(input('__ weeks/months/years: '))
+oldTimemeasurement = input('A) Weeks, B) Months, C) Years: ')
+
+
